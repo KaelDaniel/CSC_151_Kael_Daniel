95f391aa30ad66d498faea426f45220c504c1356
SANDHILLS\LH218-06 <jad.talbert@gmail.com>
2026-01-21 11:18:48 -0500

    yeet, funny fish

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,14 @@
-#Hello world
+/*
+@Author: Kael Daniel
+@Date: 1/21/2026
+@Purpose: my first java program (it is weird)
+ah so this is how you make notes
+this is my first java program, where it says Hello World */
+
+public class HelloWorld //public definition class
+{
+    public static void main(String[] args) //starts the code, string array of arguments
+    {
+        System.out.println("Hello World"); //prints the line "Hello World"
+    }
+}
