572a49c240a2d5201c576c5b732e7d0a98580c64
Kael Daniel <danielko552@mail.sandhills.edu>
2026-02-09 11:53:45 -0500

    agian vsc is being dead

--- src/labs/example/mammals/Main.java
+++ src/labs/example/mammals/Main.java
@@ -1,17 +1,17 @@
-package src.labs.example.mammals;
+package labs.example.mammals;
 
-import src.labs.example.mammals.Mammal;
+import labs.example.mammals.Mammal;
 
 public class Main{
-    public static void main(String[] args) throws Exception(
+    public static void main(String[] args) throws Exception{
         Mammal mammal = new Mammal();
 
         mammal.setName("dog");
         String myMammalName = mammal.getName();
-        System.out.println(myMammalName();
+        System.out.println(myMammalName);
 
         mammal.eat();
+
         mammal.sleep();
-        );
-    )
+    }
 }
