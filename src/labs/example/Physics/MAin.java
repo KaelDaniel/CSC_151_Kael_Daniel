b9c0b230b0896df174e0f835574453de4c0286eb
Kael Daniel <danielko552@mail.sandhills.edu>
2026-02-22 20:43:13 -0500

    Having issues with computer at home

--- src/labs/example/Physics/MAin.java
+++ src/labs/example/Physics/MAin.java
@@ -6,7 +6,10 @@ public class MAin {
     public static void main(String[] args) throws Exception{
         Physics maths = new Physics();
 
-        maths.getDistance(6, 20);
+        double distance1 = time;
+        double distance2 = lightSpeedMPH;
+
+        maths.getDistance(distance1, distance2);
         
         maths.getVelocity(12, 21);
         
@@ -28,6 +31,25 @@ public class MAin {
         
         else {
             maths.logValidAngleInfo(maths.getAngle(4, 3));
+        }
+        public void getLightSpeedinMPH(){
+            double lightSpeed = 186282;
+            double lightSpeedMPH = lightSpeed * 3600;
+            return lightSpeedMPH;
+        }
+
+        public void getTimeFromSunToEarthInHours(){
+            double lightSpeedMPH = getLightSpeedinMPH();
+            double distanceSunToEarth = 92955807.3;
+            double time = distanceSunToEarth / lightSpeedMPH;
+            return time;
+        }
+
+        if getDistance(getTimeFromSunToEarthInHours(), getLightSpeedinMPH()) > 92955807.3 {
+            System.out.println("The distance from the sun to the earth is greater than 92955807.3 miles.");
+        }
+        else {
+            System.out.println("The distance from the sun to the earth is less than 92955807.3 miles.");
         }
     }
 }
