6be287e6c5683bac8dc188466e3459af51f135e0
SANDHILLS\LH218-06 <jad.talbert@gmail.com>
2026-03-02 11:50:51 -0500

    I am staring to get how these arrays work

---
+++ src/labs/example/arrays/arrayOperations.java
@@ -0,0 +1,12 @@
+package labs.example.arrays;
+
+public class arrayOperations {
+    
+    public static void createNewArray(int x1){
+        int[] arrayLength = new int[x1];
+        int count = 0;
+        for (;count <= x1;){
+            
+        }
+    };
+}
