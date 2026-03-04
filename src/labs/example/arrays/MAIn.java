5e3e0e41291c90508b0e4bab7058281025e4a7bc
SANDHILLS\LH218-06 <jad.talbert@gmail.com>
2026-03-04 10:56:09 -0500

    got one done, had to use a loot of things to figure it out, bu tI think I understand it now

--- src/labs/example/arrays/MAIn.java
+++ src/labs/example/arrays/MAIn.java
@@ -1,5 +1,16 @@
 package labs.example.arrays;
 
+import java.util.Scanner;
+
 public class MAIn {
-    
+
+    public static void main(String[] args) {
+        arrayOperations arrays = new arrayOperations();
+
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+
+        arrays.createNewArray(input);
+    }
 }
--- src/labs/example/arrays/arrayOperations.java
+++ src/labs/example/arrays/arrayOperations.java
@@ -1,12 +1,27 @@
 package labs.example.arrays;
 
+import java.util.Scanner;
+
 public class arrayOperations {
     
-    public static void createNewArray(int x1){
+    public void createNewArray(int x1){
         int[] arrayLength = new int[x1];
-        int count = 0;
-        for (;count <= x1;){
+        for (int count = 0;count < arrayLength.length; count++){
+
+            System.out.println("assign a number to array number "+count+":");
             
+            Scanner yeetScanner = new Scanner(System.in);
+            
+            arrayLength[count] = yeetScanner.nextInt();
+        }
+        
+        System.out.println("I created a new array and it now has "+x1+" items in it");
+        
+        System.out.println("The array items and their values are listed below:");
+        
+        for (int yeet = 0; yeet < arrayLength.length; yeet++) {
+            System.out.println( yeet + ": " + arrayLength[yeet]);
         }
     };
+    
 }
