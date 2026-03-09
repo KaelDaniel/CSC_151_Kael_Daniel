28f339879b014a26bb9cf805ff7eb4c6c2b92139
Kael Daniel <danielko552@mail.sandhills.edu>
2026-03-08 23:27:07 -0400

    Add comments to MAIn.java for better clarity
    Added comments to clarify each operation in the main method.

--- src/labs/example/arrays/MAIn.java
+++ src/labs/example/arrays/MAIn.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class MAIn {
 
     public static void main(String[] args) {
-        arrayOperations arrays = new arrayOperations();
+        arrayOperations arrays = new arrayOperations(); //creates a new array for use
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //sets up the scanner system for inputs
 
-        int input = scanner.nextInt();
+        int input = scanner.nextInt(); // intiger input
 
-        arrays.createNewArray(input);
+        arrays.createNewArray(input); //calls for the public array in arrayOperations, with the given input as the value
 
-        int[] sortedArrays = new int[50];
+        int[] sortedArrays = new int[50]; //creates a new array with 50 slots
 
-        for (int num = 0; num < sortedArrays.length; num++) {
+        for (int num = 0; num < sortedArrays.length; num++) { //a for loop that sets each slot in the second array with a random number from 1 to 100
             sortedArrays[num] = (int)(Math.random() * 100);
         }
 
-        arrayOperations.sortArray(sortedArrays);
+        arrayOperations.sortArray(sortedArrays); //calls for the public class that will sort the 50 random slots in the second array
 
-        arrayOperations.getDaysAndMonths();
+        arrayOperations.getDaysAndMonths(); //calls for the array that lists the 12 months and how many days are in each one
     }
 }
