15de5cdc82d3872461b0bd0d732aa9aaf6fcbd96
SANDHILLS\LH218-06 <jad.talbert@gmail.com>
2026-04-08 11:36:25 -0400

    this makes no sense to me

--- src/labs/example/fileOperations/FileOperator.java
+++ src/labs/example/fileOperations/FileOperator.java
@@ -18,7 +18,17 @@ public class FileOperator {
     private static void open_CSV_File(){
         File csv_file = new File(File_Name);
         File csv_error = new File(Error_Logger_File);
+        try (BufferedReader reader = new BufferedReader(new FileReader(csv_file))){;
+        String[] line;
+        while ((line = reader.readLine()) != null) {
+            String[] values = line.split(",");
 
-        
+            String name = values[0];
+            int math = Integer.parseInt(values[1]);
+            int sci = Integer.parseInt(values[2]);
+            int ela = Integer.parseInt(values[3]);
+            
+        }
+        }
     }
 }
