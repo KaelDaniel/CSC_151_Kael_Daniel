30bbdde8c4e8184677d1f0d4e408863093bc77f5
SANDHILLS\LH218-06 <jad.talbert@gmail.com>
2026-04-15 10:19:08 -0400

    I don't know, why this doesn't make sense makes no sense to me

--- src/labs/example/fileOperations/FileOperator.java
+++ src/labs/example/fileOperations/FileOperator.java
@@ -7,26 +7,28 @@ import java.util.*;
 
 public class FileOperator {
     
-    private static final String File_Path = "T:\\CSC_151\\src\\labs\\example\\fileOperations\\";
+    private static final String File_Path = "T:\\CSC_151_Kael_Daniel\\src\\labs\\example\\fileOperations\\";
     private static final String File_Name = File_Path + "files\\users.csv";
     private static final String Error_Logger_File = File_Path + "logs\\csv_error.log";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         open_CSV_File();
     }
 
-    private static void open_CSV_File(){
+    private static void open_CSV_File() throws FileNotFoundException, IOException{
         File csv_file = new File(File_Name);
         File csv_error = new File(Error_Logger_File);
         try (BufferedReader reader = new BufferedReader(new FileReader(csv_file))){;
-        String[] line;
-        while ((line = reader.readLine()) != null) {
+        String line;
+        while ((line = reader.readLine()) != null ) {
             String[] values = line.split(",");
 
             String name = values[0];
             int math = Integer.parseInt(values[1]);
             int sci = Integer.parseInt(values[2]);
             int ela = Integer.parseInt(values[3]);
+            float average = (math+sci+ela)/3;
+            System.out.println(name+": "+(average));
             
         }
         }
