03b01088199878b0260951a0648f80d4bcb3c3e6
SANDHILLS\LH218-06 <jad.talbert@gmail.com>
2026-04-15 11:06:56 -0400

    just trying to do something, still don't understand what I am missing for accessing the files

--- src/labs/example/fileOperations/logger.java
+++ src/labs/example/fileOperations/logger.java
@@ -1,5 +1,28 @@
 package labs.example.fileOperations;
 
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
 public class logger {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
     
+    }
+
+    public static void openErrorLog(){
+
+    };
+
+    public static void getCountOfErrorTypes(){
+        
+    };
+
+    public static void getMemoryLimitExceededCount(){
+
+    };
+
+    private static void getDiskSpaceErrorsWithIPAddress(file){
+
+    };
 }
