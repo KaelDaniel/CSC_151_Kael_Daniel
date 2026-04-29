908eb3198533a9615d7a49dec1cdb4733999a198
jadTalbert <talbertj@sandhills.edu>
2026-04-29 15:02:27 -0400

    got it working, heel yeah, half way through week 13

--- src/labs/example/fileOperations/logger.java
+++ src/labs/example/fileOperations/logger.java
@@ -8,28 +8,53 @@ import java.util.*;
 public class logger {
 
     private static final String File_Path = "T:\\CSC_151_Kael_Daniel\\src\\labs\\example\\fileOperations\\";
-    private static final String File_Name = File_Path + "files\\users.csv";
     private static final String Error_Logger_File = File_Path + "logs\\csv_error.log";
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        openErrorLog();
+        try (BufferedReader file1 = openErrorLog()) {
+            getCountOfErrorTypes(file1);
+        }
     }
 
-    public static void openErrorLog() throws FileNotFoundException, IOException{
+    public static BufferedReader openErrorLog() throws FileNotFoundException, IOException{
         File csv_error = new File(Error_Logger_File); // creates a new file object with the path to the error log file
-        try (BufferedReader reader = new BufferedReader(new FileReader(csv_error))){
-            String error_line;
-            while ((error_line = reader.readLine()) != null){
-                System.out.println(error_line);
+        return new BufferedReader(new FileReader(csv_error));
+    }
+
+    public static void getCountOfErrorTypes(BufferedReader reader) throws IOException {
+        List<String> logLevels = new ArrayList<>();
+        String error_Line;
+        while ((error_Line = reader.readLine()) != null) {
+            String[] error_count = error_Line.split(" ");
+            if (error_count.length > 2) {
+                logLevels.add(error_count[2]);
             }
         }
-    };
+        // Count the occurrences of each log level
+        int error_Counter = 0;
+        int warn_Counter = 0;
+        int info_Counter = 0;
 
-//    public static void getCountOfErrorTypes(){
-        
-//    };
+        for (String level : logLevels) {
+            if (level.equals("[ERROR]")) {
+                error_Counter++;
 
-//    public static void getMemoryLimitExceededCount(){}
+            } 
+            else if (level.equals("[WARN]")) {
+                warn_Counter++;
+            } 
+            else if (level.equals("[INFO]")) {
+                info_Counter++;
+
+            }
+        }
+
+        System.out.println("[ERROR]: " + error_Counter);
+        System.out.println("[WARN]: " + warn_Counter);
+        System.out.println("[INFO]: " + info_Counter);
+    }
+
+//    public static void getMemoryLimitExceededCount(file2){}
     
-//    private static void getDiskSpaceErrorsWithIPAddress(file){}
+//    private static void getDiskSpaceErrorsWithIPAddress(){}
 }
